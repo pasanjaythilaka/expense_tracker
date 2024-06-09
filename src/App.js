408f831e888b3d7f4b9646aea7314912f146d018
Pasan Jayathilaka <pasan1731@gmail.com>
2024-06-09 13:02:07 +0530

    dev completed.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import {Balance} from './components/Balance';
 import {IncomeExpenses} from './components/IncomeExpenses';
 import {TransactionList} from './components/TransactionList';
 import {AddTransaction} from './components/AddTransaction';
+import {GlobalProvider} from './context/GlobalState';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header/>
-       <div className=".container">
+       <div className="container">
          <Balance />
          <IncomeExpenses/>
          <TransactionList/>
          <AddTransaction/>
        </div>
-    </div>
+    </GlobalProvider>
+   
+    
   );
 }
 
